b5b3f584c31157991486bd5cbad401548df70414
Taras Chybis <t0660804676@gmail.com>
2025-01-07 09:13:16 +0200

    Removed semicolonses and formatted indentations

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import Header from "./components/Header/Header";
+import Header from "./components/Header/Header"
 
 function App() {
    return (
-     <Header />
-  );
+      <Header />
+   );
 }
 
-export default App;
+export default App
