5e49e97bbed14ff3a78d0338aa2a566479d57e98
Taras Chybis <t0660804676@gmail.com>
2025-01-13 23:33:37 +0200

    button tags were changed to a tags in the header section. Shop, OnSale, NewArrivals, Brands pages were connected.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,21 +1,21 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
-import Home from "../../layers/Home/Home"
+import Home from '../../layers/Home/Home'
 import CasualStyle from '../../layers/CasualStyle/CasualStyle'
 import FormalStyle from '../../layers/FormalStyle/FormalStyle'
 import PartyStyle from '../../layers/PartyStyle/PartyStyle'
 import GymStyle from '../../layers/GymStyle/GymStyle'
-// import Shop from "../Pages/Home"
-// import OnSale from "../Pages/About.js"
-// import NewArrivals from "../Pages/Contacts"
-// import Brands from "../Pages/Blog"
+import Shop from '../../layers/Shop/Shop'
+import OnSale from '../../layers/OnSale/OnSale'
+import NewArrivals from '../../layers/NewArrivals/NewArrivals'
+import Brands from '../../layers/Brands/Brands'
 
-import styles from "./Header.module.scss"
-import arrowDown from "../../assets/Header-arrow-down.svg"
-import searchIcon from "../../assets/Header-search-icon.svg"
-import cartIcon from "../../assets/Header-cart-icon.svg"
-import userIcon from "../../assets/Header-user-icon.svg"
+import styles from './Header.module.scss'
+import arrowDown from '../../assets/Header-arrow-down.svg'
+import searchIcon from '../../assets/Header-search-icon.svg'
+import cartIcon from '../../assets/Header-cart-icon.svg'
+import userIcon from '../../assets/Header-user-icon.svg'
 import Container from '../Container/Container'
 
 const Header = () => {
@@ -31,21 +31,21 @@ const Header = () => {
                      </a>
                      <ul className={styles.nav__navBar}>
                         <li className={styles.nav__navBar__navBarItem}>
-                           <button className={styles.nav__navBar__navBarItem__btn} type='button'>Shop</button>
+                           <a href='/Shop' className={styles.nav__navBar__navBarItem__link}>Shop</a>
                         </li>
                         <li className={styles.nav__navBar__navBarItem}>
-                           <button className={styles.nav__navBar__navBarItem__btn} type='button'>
+                           <button className={styles.nav__navBar__navBarItem__btn} type='button' id='headerShopBtn'>
                               <img src={arrowDown} alt="arrow-down-image" width="16px" height="16px" />
                            </button>
                         </li>
                         <li className={styles.nav__navBar__navBarItem}>
-                           <button className={styles.nav__navBar__navBarItem__btn} type='button'>On Sale</button>
+                           <a href='/OnSale' className={styles.nav__navBar__navBarItem__link}>On Sale</a>
                         </li>
                         <li className={styles.nav__navBar__navBarItem}>
-                           <button className={styles.nav__navBar__navBarItem__btn} type='button'>New Arrivals</button>
+                           <a href='/NewArrivals' className={styles.nav__navBar__navBarItem__link}>New Arrivals</a>
                         </li>
                         <li className={styles.nav__navBar__navBarItem}>
-                           <button className={styles.nav__navBar__navBarItem__btn} type='button'>Brands</button>
+                           <a href='/Brands' className={styles.nav__navBar__navBarItem__link}>Brands</a>
                         </li>
                      </ul>
                   </nav>
@@ -72,6 +72,10 @@ const Header = () => {
          <Router>
             <Routes>
                <Route path="/" element={<Home />} />
+               <Route path="/Shop" element={<Shop />} />
+               <Route path="/OnSale" element={<OnSale />} />
+               <Route path="/NewArrivals" element={<NewArrivals />} />
+               <Route path="/Brands" element={<Brands />} />
                <Route path="/CasualStyle" element={<CasualStyle />} />
                <Route path="/FormalStyle" element={<FormalStyle />} />
                <Route path="/PartyStyle" element={<PartyStyle />} />
